b45fae26712ffce753490e2aa54514137860457f
Orest <124895227+OrestPokotylenko@users.noreply.github.com>
2025-04-20 16:38:00 +0200

    enities added

---
+++ src/main/java/nl/inholland/bank_api/model/entities/Account.java
@@ -0,0 +1,36 @@
+package nl.inholland.bank_api.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nl.inholland.bank_api.model.enums.AccountType;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String iban;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(15,2) DEFAULT 0.00")
+    private BigDecimal balance;
+}
