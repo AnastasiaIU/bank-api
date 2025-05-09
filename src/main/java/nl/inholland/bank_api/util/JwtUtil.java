8f66898788ad00f416b9d35aaf8bedabcd144907
nazpedawi <144240165+nazpedawi@users.noreply.github.com>
2025-05-09 15:19:40 +0200

    security and exception improvements

--- src/main/java/nl/inholland/bank_api/util/JwtUtil.java
+++ src/main/java/nl/inholland/bank_api/util/JwtUtil.java
@@ -1,15 +1,22 @@
 package nl.inholland.bank_api.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletResponse;
+import nl.inholland.bank_api.model.dto.ExceptionDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.security.Key;
 import java.util.Date;
 import java.util.Base64;
+import java.util.List;
 
 @Component
 public class JwtUtil {
@@ -40,12 +47,28 @@ public class JwtUtil {
                 .parseClaimsJws(token).getBody().getSubject();
     }
 
-    public boolean isTokenValid(String token) {
-        try {
+    public void validateToken(String token) {
             Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
+
+    public void sendJwtErrorResponse(HttpServletResponse response, JwtException e) throws IOException {
+        String message = switch (e.getClass().getSimpleName()) {
+            case "ExpiredJwtException" -> "Expired Token";
+            case "MalformedJwtException" -> "Malformed token";
+            case "SignatureException" -> "Invalid token signature";
+            case "UnsupportedJwtException" -> "Unsupported token";
+            default -> e.getMessage();
+        };
+
+        ExceptionDTO dto = new ExceptionDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                e.getClass().getSimpleName(),
+                List.of(message)
+        );
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        new ObjectMapper().writeValue(response.getWriter(), dto);
+    }
+
 }
