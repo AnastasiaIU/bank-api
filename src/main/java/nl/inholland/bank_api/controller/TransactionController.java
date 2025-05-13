cd83431a68c6683cd63c351d4fd006b6a2fd6689
Pasha <pavel.shulgat.ne@gmail.com>
2025-05-13 20:49:05 +0200

    Add filtres

--- src/main/java/nl/inholland/bank_api/controller/TransactionController.java
+++ src/main/java/nl/inholland/bank_api/controller/TransactionController.java
@@ -1,14 +1,12 @@
 package nl.inholland.bank_api.controller;
 
 import jakarta.validation.Valid;
-import nl.inholland.bank_api.model.dto.CombinedTransactionDTO;
 import nl.inholland.bank_api.model.dto.TransactionRequestDTO;
 import nl.inholland.bank_api.model.dto.TransactionResponseDTO;
 import nl.inholland.bank_api.service.TransactionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,19 +26,8 @@ public class TransactionController {
     }
 
     @GetMapping("/accounts/{accountId}/transactions")
-    public ResponseEntity<List<CombinedTransactionDTO>> getAllAccountTransactions(
-            @PathVariable Long accountId,
-            @RequestParam(required = false) String onDate,
-            @RequestParam(required = false) String before,
-            @RequestParam(required = false) String after,
-            @RequestParam(required = false) BigDecimal amount,
-            @RequestParam(required = false) String comparison,
-            @RequestParam(required = false) String sourceIban,
-            @RequestParam(required = false) String targetIban
-    ) {
-        List<CombinedTransactionDTO> transactions = transactionService.getFilteredTransactions(
-                accountId, onDate, before, after, amount, comparison, sourceIban, targetIban
-        );
+    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByAccount(@PathVariable Long accountId) {
+        List<TransactionResponseDTO> transactions = transactionService.getTransactionsForAccount(accountId);
         return ResponseEntity.ok(transactions);
     }
 }
