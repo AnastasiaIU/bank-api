129d46c21077044f1edc33f5847556bfc1533c5f
ZhadykDanylo <zhadykdanylo@gmail.com>
2025-05-16 18:29:46 +0200

    user review endpoint

--- src/main/java/nl/inholland/bank_api/model/dto/ApprovalStatusUpdateDTO.java
+++ src/main/java/nl/inholland/bank_api/model/dto/ApprovalStatusUpdateDTO.java
@@ -3,7 +3,6 @@ package nl.inholland.bank_api.model.dto;
 import nl.inholland.bank_api.model.enums.ApprovalStatus;
 import lombok.Data;
 import jakarta.validation.constraints.NotNull;
-
 import java.util.List;
 
 @Data
