29b5bf212871bf2315a5f58e25bd0149603ce346
Pasha <pavel.shulgat.ne@gmail.com>
2025-05-29 15:23:26 +0200

    Added filters

--- src/main/java/nl/inholland/bank_api/controller/TransactionController.java
+++ src/main/java/nl/inholland/bank_api/controller/TransactionController.java
@@ -1,10 +1,16 @@
 package nl.inholland.bank_api.controller;
 
 import jakarta.validation.Valid;
+import nl.inholland.bank_api.model.dto.CombinedTransactionDTO;
+import nl.inholland.bank_api.model.dto.TransactionFilterDTO;
 import nl.inholland.bank_api.model.dto.TransactionRequestDTO;
-import nl.inholland.bank_api.model.dto.TransactionResponseDTO;
+import nl.inholland.bank_api.model.entities.User;
+import nl.inholland.bank_api.service.AccountService;
 import nl.inholland.bank_api.service.TransactionService;
+import nl.inholland.bank_api.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -14,9 +20,13 @@ import java.util.List;
 @RequestMapping
 public class TransactionController {
     private final TransactionService transactionService;
+    private final UserService userService;
+    private final AccountService accountService;
 
-    public TransactionController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService, UserService userService, AccountService accountService) {
         this.transactionService = transactionService;
+        this.userService = userService;
+        this.accountService = accountService;
     }
 
     @PostMapping("/transactions")
@@ -26,8 +36,21 @@ public class TransactionController {
     }
 
     @GetMapping("/accounts/{accountId}/transactions")
-    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByAccount(@PathVariable Long accountId) {
-        List<TransactionResponseDTO> transactions = transactionService.getTransactionsForAccount(accountId);
+    public ResponseEntity<List<CombinedTransactionDTO>> getAllAccountTransactions(
+            @PathVariable Long accountId, @ModelAttribute TransactionFilterDTO transactionFilterDTO, Authentication authentication)
+    {
+        String email = authentication.getName();
+        User currentUser = userService.getUserByEmail(email);
+
+        boolean ownsAccount = accountService
+                .fetchAccountsByUserId(currentUser.getId())
+                .stream()
+                .anyMatch(account -> account.getId().equals(accountId));
+        if (!ownsAccount) {
+            throw new AccessDeniedException("You are not authorized to view these transactions.");
+        }
+        List<CombinedTransactionDTO> transactions = transactionService.getFilteredTransactions(
+                accountId, transactionFilterDTO);
         return ResponseEntity.ok(transactions);
     }
 }
